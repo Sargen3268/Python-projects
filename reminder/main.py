2596a8777a2ce051047e13d4aee2a5c511394860
Chinmay <chinmaychiplunkar3368@gmail.com>
2026-06-15 12:17:28 +0530

    timer

--- reminder/main.py
+++ reminder/main.py
@@ -1,5 +1,14 @@
+import sys
+print(sys.executable)
+
 import time
+from plyer import notification
 
 while True:
     print("This is a reminder!")
-    time.sleep(60)  # Wait for 60 seconds before printing the reminder again
+    notification.notify(
+        title="Reminder",
+        message="Drink Water.",
+        timeout=10  # Notification will disappear after 10 seconds
+    )
+    time.sleep(60*60)  # Wait for 60 seconds before printing the reminder again
